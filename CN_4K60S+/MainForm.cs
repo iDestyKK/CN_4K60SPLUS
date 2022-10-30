99fb923933f60787806928f8d87e4501c797af4f
iDestyKK <clara@claranguyen.me>
2022-10-30 00:53:18 -0400

    Made MainForm load in settings too.

--- CN_4K60S+/MainForm.cs
+++ CN_4K60S+/MainForm.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,20 +7,52 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace CN_4K60S_
 {
     public partial class MainForm : Form
     {
+        private class app_settings {
+            public app_settings() {
+                // Default Values
+                paths["FFmpeg"] = "";
+            }
+
+            public void load_if_possible(string path) {
+                // If the configuration file doesn't exist, no worries.
+                if (!File.Exists("settings.xml")) {
+                    return;
+                }
+
+                // Load in the XML file
+                XDocument xdoc = XDocument.Load("settings.xml");
+
+                paths["FFmpeg"] = xdoc.Element("Paths").Element("FFmpeg").Value;
+            }
+
+            public void save(string path) {
+                // Scrapped. No reason for this to existing(?)
+            }
+
+            public Dictionary<string, string> paths = new Dictionary<string, string>();
+        }
+
+        private app_settings _settings = new app_settings();
+
         public MainForm()
         {
             InitializeComponent();
+            _settings.load_if_possible("settings.xml");
         }
 
         private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Form settings = new Settings();
             settings.ShowDialog();
+
+            // Once done, refresh settings
+            _settings.load_if_possible("settings.xml");
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
